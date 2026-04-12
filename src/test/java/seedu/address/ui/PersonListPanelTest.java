e10f0cb552b3c7e25f158fbef7a5f1d831a5e519
Kawai Cheng <cheng.kw0123@gmail.com>
2026-04-12 16:24:58 +0800

    Fix checkstyle issues

--- src/test/java/seedu/address/ui/PersonListPanelTest.java
+++ src/test/java/seedu/address/ui/PersonListPanelTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.time.LocalDate;
 import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 import seedu.address.testutil.PersonBuilder;
